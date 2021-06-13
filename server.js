0e75ddd30ba09d8d50d03cbc1746732e9aaf3d1a
NielsPeeters96 <78353674+NielsPeeters96@users.noreply.github.com>
2021-06-13 12:10:02 +0200

    navigation

--- server.js
+++ server.js
@@ -10,23 +10,26 @@ const goals = require('./routes/goals');
 const { response } = require('express');
 
 app.use(express.static(path.resolve("public")))
-    .use(compression());
+.use(compression());
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
 app.get("/", home, (req, res) => {
-    res.render("profiel.ejs");
-})
+    res.render('profiel.ejs');
+});
 
-app.get("/activiteiten", (req, res) => {
-    res.render("activiteiten.ejs")
-})
+app.get('/activiteiten', (req, res) => {
+    res.render('activiteiten.ejs');
+});
 
-app.get("/doelen", goals, (req, res) => {
-    res.render("doelen.ejs");
-})
+app.get('/doelen', goals, (req, res) => {
+    res.render('doelen.ejs');
+});
 
-app.get("/logboek", (req, res) => {
-    res.render("logboek.ejs");
-})
+app.get('/logboek', (req, res) => {
+    res.render('logboek.ejs');
+});
 
 app.listen(port, () => {
     console.log(`Server is working at http://localhost:${port}`)
