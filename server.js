717086a221d69b88ac86b17942c6ffcbb38f24c3
Niels Peeters <niels.peeters@hva.nl>
2021-06-23 08:50:23 +0200

    setup database

--- server.js
+++ server.js
@@ -2,14 +2,37 @@ const express = require('express');
 const compression = require('compression');
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 5000
-
+const port = process.env.PORT || 5000;
+var bodyParser = require('body-parser');
 
 const home = require('./routes/home');
 const goals = require('./routes/goals');
 const { response } = require('express');
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://npeeters:test123@logboek.tv8ou.mongodb.net/logboek-activiteiten?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+    try {
+      await client.connect({useUnifiedTopology: true});
+      const database = client.db('logboek-activiteiten');
+      const movies = database.collection('datums');
+      // Query for a movie that has the title 'Back to the Future'
+      const movie = await movies.find();
+      console.log(movie);
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 app.use(express.static(path.resolve("public")))
+.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
 .use(compression());
 
 app.set('views', './views');
