edc37e105783e948cfeafeaeae7095dd8dcd419d
Niels Peeters <niels.peeters@hva.nl>
2021-06-28 12:10:46 +0200

    database

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const compression = require('compression');
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const home = require('./routes/home');
 const goals = require('./routes/goals');
@@ -20,57 +20,76 @@ mongo.MongoClient.connect(
         if (err) {
             throw err;
         }
-        
+
         db = client.db('logboek-activiteiten');
         console.log("Connected correctly to MongoDB server");
     }
+);
+
+app.use(express.static(path.resolve("public")))
+.use(compression());
+
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get("/", home)
+.get("/activiteiten", activiteiten)
+.get("/doelen", goals)
+.get("/planactiviteit", planactiviteiten)
+.get("/logboek", logboek)
+
+.post("/logboek", add)
+
+function add(req, res, next) {
+    const datum = getDate();
+    const activiteitenobject = {wandelen: req.body.wandelen, 
+        fietsen: req.body.fietsen,
+        tuinieren: req.body.tuinieren,
+        boodschappen: req.body.boodschappen,
+        oefeningen: req.body.oefeningen,}
+    let activiteitenlijst = []
+    for (const [key, value] of Object.entries(activiteitenobject)) {
+        if (value) {
+            activiteitenlijst.push(key)
+        }
+      }
+    db.collection("datums").insertOne({
+        datum,
+        activiteitenlijst, 
+
+        },
+        renderPage()
     );
-    app.use(express.static(path.resolve("public")))
-        .use(compression());
-        
-        app.set('views', './views');
-        app.set('view engine', 'ejs');
-        
-        app.get("/", home)
-        .get("/activiteiten", activiteiten)
-        .get("/doelen", goals)
-        .get("/planactiviteit", planactiviteiten)
-        .get("/logboek", logboek)
 
-        .post("/logboek", add)
-        
-        function add(req, res, next) {
-            db.collection("datums").insertOne({
-                wandelen: req.body.wandelen,
-                fietsen: req.body.fietsen,
-                tuinieren: req.body.tuinieren,
-                boodschappen: req.body.boodschappen,
-                oefeningen: req.body.oefeningen, // zet alles na de ? uit, dan krijg je een data object. Daar kan je meer mee.
-            },
-            renderPage()
-            );
-            
-            function renderPage(err) {
-                if (err) {
-                    next(err);
-                } else {
-                    res.redirect("/logboek");
-                }
-            }
+    function renderPage(err) {
+        if (err) {
+            next(err);
+        } else {
+            res.redirect("/");
         }
+    }
+}
 
-        function logboek(req, res) {
-            res.render("logboek.ejs")
-        }
-        
-        function planactiviteiten(req, res) {
-            res.render("planactiviteit.ejs")
-        }
-        
-        function activiteiten(req, res) {
-            res.render("activiteiten.ejs")
-        }
-        
-        app.listen(PORT, () => {
-            console.log(`Server is working at http://localhost:${PORT}`)
-        });
+function getDate() {
+    let yourDate = new Date()
+yourDate.toISOString().split('T')[0]
+    const offset = yourDate.getTimezoneOffset()
+yourDate = new Date(yourDate.getTime() - (offset*60*1000))
+return yourDate.toISOString().split('T')[0]
+}
+
+async function logboek(req, res) {
+    const datums = await db.collection("datums").find().toArray()
+    const day = getDate()
+    res.render("logboek.ejs", {datums, day})
+}
+
+function planactiviteiten(req, res) {
+    res.render("planactiviteit.ejs")
+}
+
+function activiteiten(req, res) {
+    res.render("activiteiten.ejs")
+}
+
+app.listen(PORT, () => console.log(`App is running on port ${PORT}`));
