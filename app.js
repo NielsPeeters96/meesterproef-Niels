0b77a2280bbd6cccb20fdbd5237a22c960297569
Niels Peeters <niels.peeters@hva.nl>
2021-06-28 11:48:01 +0200

    port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const compression = require('compression');
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT = 3000;
+const PORT = process.env.PORT
 
 const home = require('./routes/home');
 const goals = require('./routes/goals');
